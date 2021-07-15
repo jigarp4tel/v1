e2b6d32210ad5121790b83c3e738f82c79f815c0
Jigar Patel <44246251+jigarp4tel@users.noreply.github.com>
2021-07-14 23:54:41 -0500

    -updated content

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,55 @@ function toggleSkills() {
 skillsHeader.forEach((e) => {
     e.addEventListener('click', toggleSkills)
 })
+
+
+/* ============= PORTFOLIO SWIPER ======================= */
+
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+});
+
+/* ============= DARK LIGHT THEME ======================= */
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon())
+
+    console.log(document.body.classList.contains(darkTheme))
+    console.log(getCurrentTheme())
+})
